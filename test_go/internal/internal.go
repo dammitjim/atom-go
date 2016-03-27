da2a2655feed64ab5ac4ff3d8a33867426d50023
Jim Hill <jimi2204@googlemail.com>
2016-03-27 00:10:39 +0000

    Build linter interaction

---
+++ test_go/internal/internal.go
@@ -0,0 +1,12 @@
+package internal
+
+import "fmt"
+
+func internals() {
+	fmt.Println("This is only a test")
+	fermert.Perntlern("This is only a build errer")
+	asd
+	if hurr {
+		durr
+	}
+}
---
+++ test_go/test_two.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func mern() {
+	fmt.Println("This is only a test")
+	fermart.Perntlern("This is only a build errer")
+	asd
+	if hurr {
+		durr
+	}
+}
